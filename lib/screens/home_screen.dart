8d36b5385ba994b0286496815447cea4fa0bdb25
marcos-bah <marcos.barbosa@unifei.edu.br>
2020-06-29 16:05:44 -0300

    finalizaçao da tela de pedidos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_to_hero/tabs/home_tab.dart';
+import 'package:loja_to_hero/tabs/orders_tab.dart';
 import 'package:loja_to_hero/tabs/products_tab.dart';
 import 'package:loja_to_hero/widgets/cart_button.dart';
 import 'package:loja_to_hero/widgets/custom_drawer.dart';
@@ -25,6 +26,15 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
           floatingActionButton: CartButton(),
+        ),
+        Container(color: Colors.yellow),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Meus Pedidos"),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
         ),
       ],
     );
